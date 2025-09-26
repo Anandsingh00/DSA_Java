6bc83e6bc1aa9c04e1599c49a8faa6bb8f3d5ad8
Anand Raj Singh <singhanand31019@gmail.com>
2025-09-26 08:11:03 +0530

    ..

--- DynamicProgramming/FriendsPairingProblem.java
+++ DynamicProgramming/FriendsPairingProblem.java
@@ -1,15 +1,27 @@
 package DynamicProgramming;
 
+import java.util.Arrays;
+import java.util.Scanner;
+
 public class FriendsPairingProblem {
-	
-	public static int pairs(int n){
-		if(n==0 || n==1 ||n==2)return n;
-		
-		return n*(pairs(n-1) + (n-1)*pairs(n-2));
+
+	public static int pairs(int n, int[] dp) {
+		if (n<=2)
+			return n;
+		if (dp[n] != -1)
+			return dp[n];
+		return dp[n] = pairs(n - 1, dp) + (n - 1) * pairs(n - 2, dp);
 	}
-	public static void main(String[]args) {
-		int ans = pairs(4);
-		System.out.println(ans);
+
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		System.out.print("Enter n : ");
+		int n = sc.nextInt();
+		int dp[] = new int[n + 1];
+		Arrays.fill(dp, -1);
+		int ans = pairs(n, dp);
+
+		System.out.println("Output: " + ans);
 	}
 
 }
