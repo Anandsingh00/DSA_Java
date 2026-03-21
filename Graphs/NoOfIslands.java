be6eabf9ab9429f158b9a766a19361cef76fd46e
Anand Raj Singh <singhanand31019@gmail.com>
2026-03-21 18:52:51 +0530

    add the files

---
+++ Graphs/NoOfIslands.java
@@ -0,0 +1,90 @@
+package Graphs;
+
+import java.util.LinkedList;
+import java.util.Queue;
+
+public class NoOfIslands {
+    public static int numIslands(char[][] grid) {
+            int m = grid.length, n = grid[0].length;
+//        make  a boolean vis[][] array
+        boolean vis [][] = new boolean[m][n];
+        int count = 0;
+
+        for(int i = 0 ; i < m ; i++){
+            for(int j = 0 ; j < n ;j++){
+                if(grid[i][j] == '1' && !vis[i][j] ){
+                    bfs(i,j,grid,vis);
+                    vis[i][j]= true;
+                    count++;
+                }
+            }
+        }
+
+        return count;
+
+    }
+
+    static class Pair{
+        int row;
+        int col;
+
+        Pair(int row , int col){
+            this.row = row;
+            this.col = col;
+        }
+    }
+
+    public static  void bfs(int i , int j , char[][] grid,boolean [][] vis){
+        int m = grid.length;
+        int n = grid[0].length;
+        Queue<Pair> q = new LinkedList<>();
+        q.add(new Pair(i,j));
+
+        while(!q.isEmpty()){
+            Pair front = q.remove();
+            int row = front.row, col= front.col;
+
+            //up: row-1,col
+            if(row > 0){
+                if(vis[row-1][col] == false && grid[row-1] [col] == '1'){
+                    q.add(new Pair(row-1,col));
+                    vis[row-1] [col] = true ;
+                }
+            }
+            //down: row+1,col
+            if ((row + 1) < m){
+                if(vis[row+1][col] == false && grid[row+1][col] == '1'){
+                    q.add(new Pair(row+1,col));
+                    vis[row+1][col] = true;
+                }
+            }
+
+            //left: row,col-1
+            if (col > 0){
+                if(vis[row][col-1] == false && grid[row][col-1] == '1'){
+                    q.add(new Pair(row,col-1));
+                    vis[row][col-1] = true;
+                }
+            }
+            //right: row,col+1
+            if ((col+1) < n){
+                if(vis[row][col+1] == false && grid[row][col+1] == '1'){
+                    q.add(new Pair(row,col+1));
+                    vis[row][col+1] = true;
+                }
+            }
+        }
+    }
+
+    public static void main(String[] args) {
+        char[][] grid = {
+                {'1','1','1','1','0'},
+                {'1','1','0','1','0'},
+                {'1','1','0','0','0'},
+                {'0','0','0','0','0'}
+        };
+
+        int result = numIslands(grid);
+        System.out.println("No of Islands: " + result);
+    }
+}
---
+++ Graphs/PathExistInGraph.java
@@ -0,0 +1,46 @@
+package Graphs;
+
+import java.util.ArrayList;
+import java.util.LinkedList;
+import java.util.List;
+import java.util.Queue;
+
+public class PathExistInGraph {
+    public boolean validPath(int n, int[][] edges, int source, int end) {
+        boolean [] vis = new boolean[n];
+        List<List<Integer>> adj = new ArrayList<>(n);
+
+        for(int i = 0 ; i < n; i++){
+            List<Integer> list = new ArrayList<>();
+            adj.add(list);
+        }
+
+        for(int i = 0 ;i < edges.length ; i++){
+            int a = edges[i][0];
+            int b = edges[i][1];
+            adj.get(a).add(b);
+            adj.get(b).add(a);
+        }
+        vis[source] = true;
+        bfs(source,adj,vis);
+
+        if(!vis[end]){
+            return false;
+        }
+        return true;
+    }
+    private void bfs(int start , List<List<Integer>> adj, boolean [] vis){
+        Queue<Integer> q = new LinkedList<>();
+        q.add(start);
+        while(!q.isEmpty()){
+            int front = q.remove();
+            for(int ele: adj.get(front)){
+                if(!vis[ele]){
+                    q.add(ele);
+                    vis[ele] = true;
+                }
+            }
+        }
+
+    }
+}
